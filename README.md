67c6edaeb85639d96e27d58298ce6eca52cc1457
DerkNiessink <94176246+DerkNiessink@users.noreply.github.com>
2022-01-18 01:58:07 +0100

    updates to zweer.py
    Got started on the plotting algorithm and already implemented 6 lines!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Maas-en-Derk-open-source-
-open source
+This code is open source !
+You have to install matplotlib.pyplot to run this code hahahaha.
+
+In command line:
+pip install matplotlib.pyplot
+pip install numpy
--- zweer.py
+++ zweer.py
@@ -1,1 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+coords = {"coords{}".format(i): [] for i in range(4)}
+
+coords["coords1"] = [(4, y) for y in np.arange(-4, 16, 0.01)]
+coords["coords2"] = [(x, 2 * x + 8) for x in np.arange(4, 6, 0.01)]
+coords["coords3"] = [(x, 20) for x in np.arange(6, 15, 0.01)]
+coords["coords4"] = [(x, -(2 / 3) * x + 30) for x in np.arange(15, 18, 0.01)]
+coords["coords5"] = [(x, -2 * x + 54) for x in np.arange(18, 19, 0.01)]
+coords["coords6"] = [((y - 282) / (-14), y) for y in np.arange(14.923, 16, 0.01)]
+
+for line in coords:
+    x = []
+    y = []
+    for coord in coords[line]:
+        x.append(coord[0])
+        y.append(coord[1])
+    plt.plot(x, y)
+
+plt.show()
+
+"""
+
+
+x5 = [coord[0] for coord in coords5]
+y5 = [coord[1] for coord in coords5]
+
+
+x6 = [coord[0] for coord in coords6]
+y6 = [coord[1] for coord in coords6]
+
+coords7 = [(x, -14 * x + 282) for x in np.arange(4, 11.333, 0.1)]
+x7 = [coord[0] for coord in coords7]
+y7 = [coord[1] for coord in coords7]
+
+coords8 = [(x, 8 * x - 155) for x in np.arange(2, 4, 0.1)]
+x8 = [coord[0] for coord in coords8]
+y8 = [coord[1] for coord in coords8]
+
+"""
